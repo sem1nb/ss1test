24eb074cf7ea73ef36bc918579a42de343cca29c
sem1nb <44867535+sem1nb@users.noreply.github.com>
2020-01-27 14:34:23 +0200

    Update t.sh

--- t.sh
+++ t.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir ~/.semaphore-cache/testfolder
+mkdir -p ~/.semaphore-cache/testfolder
 echo "one" > ~/.semaphore-cache/testfolder/one.txt
 echo "two" > ~/.semaphore-cache/testfolder/two.plt
 echo "DONE"
