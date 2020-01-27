b6ffb0a0854a5991cad91c2f94b250eac526848a
sem1nb <44867535+sem1nb@users.noreply.github.com>
2020-01-27 14:24:41 +0200

    Update t.sh

--- t.sh
+++ t.sh
@@ -1,7 +1,1 @@
-sudo locale-gen nb_NO
-sudo locale-gen nb_NO.UTF-8
-sudo update-locale
-sudo service postgresql restart
-psql -c 'CREATE COLLATION "nb_NO" (LOCALE = "nb_NO.utf8");' -d public
-sudo service postgresql restart
-psql -U runner postgres -f structure.txt
+ls -lah
